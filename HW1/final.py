5b672d5fb37d754b1b95408611147f33d4762fd8
Pranav Jadhav <pranavbjadhav@gmail.com>
2023-02-07 19:41:16 -0800

    Combination remaining

---
+++ HW1/final.py
@@ -0,0 +1,41 @@
+def main():
+    input_file = open("input.txt", "r")
+    lines = input_file.readlines()
+    data = [line.strip() for line in lines]
+
+    algorithm = data[0] #algortihm to be used
+    data[1] = data[1].split(" ")
+    width, height = int(data[1][0]), int(data[1][1]) #get height and width of mtrx3
+
+    data[2] = data[2].split(" ")
+    start_X, start_Y = int(data[2][0]), int(data[2][1]) #get starting coordinates
+
+    stamina = int(data[3])
+
+    no_of_lodges = int(data[4])
+
+    lodges_coordinates = []
+
+    for i in range(0, no_of_lodges):
+        data[4+1+i] = data[4+1+i].split(" ")
+        temp = (int(data[4+1+i][0]), int(data[4+1+i][1]))
+        lodges_coordinates.append(temp)
+        i = i+1
+
+    mtrx = []
+
+    for i in range(0, height):
+        idx = 4 + no_of_lodges + i + 1
+        data[idx] = data[idx].split(" ")
+        res = [eval(i) for i in data[idx]]
+        mtrx.append(res)
+        i = i + 1
+
+    output_file = fp = open('output.txt', 'w')
+
+    if algorithm == "BFS":
+        breadthFirstSearch(start_X, start_Y, stamina, lodges_coordinates, mtrx, height, width)
+    elif algorithm == "UCS":
+        uniformCostSearch(start_X, start_Y, stamina, lodges_coordinates, mtrx, height, width)
+    else:
+        
